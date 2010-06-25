6183698242283dea9d3be7311b40ba9e916ddfb5
Athurg Gooth <athurg@gooth.cn>
2010-06-25 14:01:58 +0800

    Port G200WO RTC Driver From MPC8313 to LPC3250

--- drivers/g200wo/rtc.c
+++ drivers/g200wo/rtc.c
@@ -17,177 +17,125 @@
 #include <asm/io.h>
 #include <asm/uaccess.h>
 #include <linux/semaphore.h>
-#include "rtc.h"
 
-//------------------------------------------------------------------------------
-// configuration
-//------------------------------------------------------------------------------
+#include <mach/platform.h>
+#include <mach/lpc32xx_rtc.h>
 
-//#define CLK_IN          (333330000/2) //333MHz
-#define CLK_IN          (266664000/2) //266MHz
-#define REG_BASE        0xE0000300
-#define REG_SIZE        0x20
-#define OFF_RTCNR       0x00
-#define OFF_RTLDR       0x01
-#define OFF_RTPSR       0x02
-#define OFF_RTCTR       0x03
-#define OFF_RTEVR       0x04
-#define OFF_RTALR       0x05
+#include "hardware.h"
 
-#define DEFAULT_RTCNR   0x00000080
-#define DEFAULT_RTPSR   (CLK_IN-1)
-
-//------------------------------------------------------------------------------
-// structure
-//------------------------------------------------------------------------------
 struct rtc_st
 {
-    struct cdev cdev;
-    struct semaphore sem;
-    volatile unsigned int __iomem *regp;
+	struct cdev cdev;
+	struct semaphore sem;
 };
 
-//------------------------------------------------------------------------------
-// global
-//------------------------------------------------------------------------------
 struct rtc_st *rtc_stp;
-
-//------------------------------------------------------------------------------
-// io functions
-//------------------------------------------------------------------------------
-static inline u32 rtc_in(unsigned int off)
-{
-    return __raw_readl(&rtc_stp->regp[off]);
-}
-
-static inline void rtc_out(unsigned int off, u32 val)
-{
-    __raw_writel(val, &rtc_stp->regp[off]);
-}
-
-//------------------------------------------------------------------------------
-// module functions
-//------------------------------------------------------------------------------
-static int rtc_open(struct inode *inode, struct file *filp)
-{
-    return 0;
-}
-
-static int rtc_release(struct inode *inode, struct file *filp)
-{
-    return 0;
-}
 
 static ssize_t rtc_write(struct file *filp, const char __user *buf, size_t size, loff_t *ppos)
 {
-    time_t rtc_time;
-    if (down_interruptible(&rtc_stp->sem))
-        return - ERESTARTSYS;
-    if (copy_from_user(&rtc_time, buf, sizeof(time_t)))
-    {
-        printk("BSP: %s fail copy_from_user\n", __FUNCTION__);
-        up(&rtc_stp->sem);
-        return  - EFAULT;
-    }
-    //rtc_stp->regp[OFFSET_SYSTEM_RTC] = rtc_time;
-    //
-    rtc_out(OFF_RTCNR, 0x0);//stop rtc
-    rtc_out(OFF_RTLDR, rtc_time);
-    rtc_out(OFF_RTCNR, DEFAULT_RTCNR);//start rtc
-    up(&rtc_stp->sem);
-    return sizeof(time_t);
+	time_t rtc_time;
+	if (down_interruptible(&rtc_stp->sem))
+		return - ERESTARTSYS;
+
+	if (copy_from_user(&rtc_time, buf, sizeof(time_t))){
+		printk("BSP: %s fail copy_from_user\n", __FUNCTION__);
+		up(&rtc_stp->sem);
+		return  - EFAULT;
+	}
+
+	//stop rtc -> load value -> start rtc
+	__raw_writel(RTC_CNTR_DIS, io_p2v(RTC_CTRL(RTC_BASE)));
+	__raw_writel(rtc_time, io_p2v(RTC_UCOUNT(RTC_BASE)));
+	__raw_writel(0, io_p2v(RTC_CTRL(RTC_BASE)));
+
+	up(&rtc_stp->sem);
+	return sizeof(time_t);
 }
 
 static ssize_t rtc_read(struct file *filp, char __user *buf, size_t size, loff_t *ppos)
 {
-    time_t rtc_time;
-    if (down_interruptible(&rtc_stp->sem))
-        return - ERESTARTSYS;
-    rtc_time = rtc_in(OFF_RTCTR);
-    if (copy_to_user(buf, &rtc_time, sizeof(time_t)))
-    {
-        printk("BSP: %s fail copy_to_user\n", __FUNCTION__);
-        up(&rtc_stp->sem);
-        return - EFAULT;
-    }
-    up(&rtc_stp->sem);
-    return sizeof(time_t);
+	time_t rtc_time;
+
+	if (down_interruptible(&rtc_stp->sem))
+		return - ERESTARTSYS;
+	
+	rtc_time = __raw_readl(io_p2v(RTC_UCOUNT(RTC_BASE)));
+	if (copy_to_user(buf, &rtc_time, sizeof(time_t))){
+		printk("BSP: %s fail copy_to_user\n", __FUNCTION__);
+		up(&rtc_stp->sem);
+		return - EFAULT;
+	}
+
+	up(&rtc_stp->sem);
+	return sizeof(time_t);
 }
 
-//------------------------------------------------------------------------------
-// register module
-//------------------------------------------------------------------------------
+
 static const struct file_operations rtc_fops = {
-        .owner  = THIS_MODULE,
-        .open   = rtc_open,
-        .release= rtc_release,
-        .read   = rtc_read,
-        .write  = rtc_write,
+	.owner  = THIS_MODULE,
+	.open   = NULL,
+	.release= NULL,
+	.read   = rtc_read,
+	.write  = rtc_write,
 };
 
 static int __init rtc_init(void)
 {
-    int ret = 0, err = 0;
-    dev_t devno;
-    // register chrdev
-    devno = MKDEV(MAJ_RTC, MIN_RTC);
-    ret = register_chrdev_region(devno, 1, NAME_RTC);
-    if (ret<0)
-    {
-        printk("BSP: %s fail register_chrdev_region\n", __FUNCTION__);
-        return ret;
-    }
-    // alloc dev
-    rtc_stp = kmalloc(sizeof(struct rtc_st), GFP_KERNEL);
-    if (!rtc_stp)
-    {
-        ret = - ENOMEM;
-        goto fail_malloc;
-    }
-    memset(rtc_stp, 0, sizeof(struct rtc_st));
-    init_MUTEX(&rtc_stp->sem);
-    // add cdev
-    cdev_init(&rtc_stp->cdev, &rtc_fops);
-    rtc_stp->cdev.owner = THIS_MODULE;
-    rtc_stp->cdev.ops = &rtc_fops;
-    err = cdev_add(&rtc_stp->cdev, devno, 1);
-    if (err)
-    {
-        printk("BSP: %s fail cdev_add\n", __FUNCTION__);
-        goto fail_remap;
-    }
-    // ioremap
-    rtc_stp->regp = ioremap(REG_BASE, REG_SIZE);
-    if (rtc_stp->regp==NULL)
-    {
-        printk("BSP: %s fail ioremap\n", __FUNCTION__);
-        goto fail_remap;
-    }
-    rtc_out(OFF_RTCNR, 0x0); //stop rtc
-    rtc_out(OFF_RTLDR, 0x0); //must write a value, otherwise rtc RTCNR can not start the RTC
-    rtc_out(OFF_RTPSR, DEFAULT_RTPSR);
-    rtc_out(OFF_RTCNR, DEFAULT_RTCNR);//start rtc
-    printk("NTS RTC Driver installed\n");
-    return 0;
+	int ret = 0, err = 0;
+	dev_t devno;
+
+	// register chrdev
+	devno = MKDEV(MAJ_RTC, MIN_RTC);
+	ret = register_chrdev_region(devno, 1, "g200wo_rtc");
+	if (ret<0){
+		printk("BSP: %s fail register_chrdev_region\n", __FUNCTION__);
+		return ret;
+	}
+
+	// alloc dev
+	rtc_stp = kmalloc(sizeof(struct rtc_st), GFP_KERNEL);
+	if (!rtc_stp){
+		ret = - ENOMEM;
+		goto fail_malloc;
+	}
+	memset(rtc_stp, 0, sizeof(struct rtc_st));
+
+	init_MUTEX(&rtc_stp->sem);
+
+	// add cdev
+	cdev_init(&rtc_stp->cdev, &rtc_fops);
+	rtc_stp->cdev.owner = THIS_MODULE;
+	rtc_stp->cdev.ops = &rtc_fops;
+	err = cdev_add(&rtc_stp->cdev, devno, 1);
+	if (err){
+		printk("BSP: %s fail cdev_add\n", __FUNCTION__);
+		goto fail_remap;
+	}
+
+	//initial RTC
+	__raw_writel(0, io_p2v(RTC_CTRL(RTC_BASE)));
+
+	printk("NTS RTC Driver installed\n");
+	return 0;
 
 fail_remap:
-    kfree(rtc_stp);
+	kfree(rtc_stp);
 
 fail_malloc:
-    unregister_chrdev_region(devno, 1);
-    printk("Fail to install NTS RTC driver\n");
-    return ret;
+	unregister_chrdev_region(devno, 1);
+	printk("Fail to install NTS RTC driver\n");
+	return ret;
 }
 
 static void __exit rtc_exit(void)
 {
-    dev_t devno;
-    iounmap(rtc_stp->regp);
-    cdev_del(&rtc_stp->cdev);
-    kfree(rtc_stp);
-    devno = MKDEV(MAJ_RTC, MIN_RTC);
-    unregister_chrdev_region(devno, 1);
-    printk("NTS RTC Driver removed\n");
+	dev_t devno;
+
+	cdev_del(&rtc_stp->cdev);
+	kfree(rtc_stp);
+	devno = MKDEV(MAJ_RTC, MIN_RTC);
+	unregister_chrdev_region(devno, 1);
+	printk("NTS RTC Driver removed\n");
 }
 
 module_init(rtc_init);
--- drivers/g200wo/rtc.h
+++
@@ -1,6 +0,0 @@
-#ifndef __RTC_H__
-#define __RTC_H__
-
-#include "hw_cfg.h"
-
-#endif // __RTC_H__
