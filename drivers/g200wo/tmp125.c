b93f0b3afcb2d83c17c89eb2f3e1baab8b15f058
Athurg Gooth <athurg@gooth.cn>
2010-07-01 10:44:49 +0800

    TMP125：调整时序

--- drivers/g200wo/tmp125.c
+++ drivers/g200wo/tmp125.c
@@ -5,26 +5,22 @@
  :: ::   ::       ::         ::         Project    : G200WO
  ::  ::  ::       ::           :::      File Name  : tmp125.c
  ::   :: ::       ::             ::     Generate   : 2009.05.31
- ::    ::::       ::       ::      ::   Update     : 2010.06.22
+ ::    ::::       ::       ::      ::   Update     : 2010-07-01 10:44:20
 ::::    :::     ::::::      ::::::::    Version    : v0.1
 
 Description
 	None
 */
-//------------------------------------------------------------------------------
-// include
-//------------------------------------------------------------------------------
 #include <linux/fs.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
 #include <linux/cdev.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/delay.h>
+
 #include <asm/io.h>
 #include <asm/uaccess.h>
-#include <linux/semaphore.h>	//semaphore Define
+#include <linux/semaphore.h>
 
-#include "hardware.h"	//Hardware Regs Addr Define
+#include "hardware.h"
 
 struct tmp125_st
 {
@@ -49,25 +45,27 @@ static ssize_t tmp125_read(struct file *filp, char __user *buf, size_t size, lof
 		return - ERESTARTSYS;
 	
 	//Chip Select active
-	tmp125_io_write(TMP125_CS_N | TMP125_SCLK, 0);
+	tmp125_io_write(TMP125_CS_N, 0);
 
 	//Lead zero bit
-	tmp125_io_write(TMP125_SCLK, 1);
 	tmp125_io_write(TMP125_SCLK, 0);
+	tmp125_io_write(TMP125_SCLK, 1);
 
 	//10 bits valid data
 	for(i=0; i<10; i++){
+		tmp125_io_write(TMP125_SCLK, 0);
 		tmp125_io_write(TMP125_SCLK, 1);
+
 		if(TMP125_DOUT & __raw_readb(GPIO_P3_INP_STATE(GPIO_BASE)))
 			data += 1;
-		tmp125_io_write(TMP125_SCLK, 0);
+
 		data <<=1;
 	}
 
 	//5 bits dummy
 	for(i=0; i<5; i++){
-		tmp125_io_write(TMP125_SCLK, 1);
 		tmp125_io_write(TMP125_SCLK, 0);
+		tmp125_io_write(TMP125_SCLK, 1);
 	}
 
 	//Chip Select inactive
@@ -78,7 +76,9 @@ static ssize_t tmp125_read(struct file *filp, char __user *buf, size_t size, lof
 		up(&tmp125_stp->sem);
 		return - EFAULT;
 	}
+
 	up(&tmp125_stp->sem);
+	
 	return sizeof(int);
 }
 
@@ -93,18 +93,18 @@ static const struct file_operations tmp125_fops = {
 
 static int __init tmp125_init(void)
 {
-	int ret = 0, err = 0;
+	int ret = 0;
 	dev_t devno;
 
-	// register chrdev
+	// register chrdev number
 	devno = MKDEV(MAJ_TMP125, MIN_TMP125);
-	ret = register_chrdev_region(devno, 1, "tmp125");
+	ret = register_chrdev_region(devno, 1, "g200wo_tmp125");
 	if (ret<0){
 		printk("BSP: %s fail register_chrdev_region\n", __FUNCTION__);
 		return ret;
 	}
 
-	// alloc dev
+	// alloc and initial memory
 	tmp125_stp = kmalloc(sizeof(struct tmp125_st), GFP_KERNEL);
 	if (!tmp125_stp){
 		ret = - ENOMEM;
@@ -113,29 +113,29 @@ static int __init tmp125_init(void)
 
 	memset(tmp125_stp, 0, sizeof(struct tmp125_st));
 	init_MUTEX(&tmp125_stp->sem);
-	
-	// add cdev
 	cdev_init(&tmp125_stp->cdev, &tmp125_fops);
 	tmp125_stp->cdev.owner = THIS_MODULE;
 	tmp125_stp->cdev.ops = &tmp125_fops;
-	err = cdev_add(&tmp125_stp->cdev, devno, 1);
-	if (err){
+	
+	// add cdev
+	rtn = cdev_add(&tmp125_stp->cdev, devno, 1);
+	if (rtn){
 		printk("BSP: %s fail cdev_add\n", __FUNCTION__);
-		goto fail_remap;
+		goto fail_add;
 	}
 
 	// set port mux
-	__raw_writel((_BIT(22) | _BIT(18) | _BIT(16) ),
-			GPIO_P3_MUX_CLR(GPIO_BASE));
+	__raw_writel((_BIT(22) | _BIT(18) | _BIT(16)), GPIO_P3_MUX_CLR(GPIO_BASE));
 
-	printk("G200WO TMP125 Driver instaltmp125\n");
+	printk("G200WO TMP125 Driver installed\n");
 	return 0;
 
-fail_remap:
+fail_add:
 	kfree(tmp125_stp);
 
 fail_malloc:
 	unregister_chrdev_region(devno, 1);
+
 	printk("Fail to install G200WO TMP125 driver\n");
 	return ret;
 }
@@ -143,10 +143,13 @@ fail_malloc:
 static void __exit tmp125_exit(void)
 {
 	dev_t devno;
+
 	cdev_del(&tmp125_stp->cdev);
 	kfree(tmp125_stp);
+
 	devno = MKDEV(MAJ_TMP125, MIN_TMP125);
 	unregister_chrdev_region(devno, 1);
+
 	printk("G200WO TMP125 Driver removed\n");
 }
 
@@ -154,5 +157,5 @@ module_init(tmp125_init);
 module_exit(tmp125_exit);
 
 MODULE_AUTHOR("Athurg.Feng, <athurg.feng@nts-intl.com>");
-MODULE_DESCRIPTION("G200WO Status TMP125");
+MODULE_DESCRIPTION("G200WO TMP125 temperature driver");
 MODULE_LICENSE("GPL");
