281aedda9d3025d67f2f2a1e01afc0ee2c5bed42
Athurg Gooth <athurg@gooth.cn>
2010-10-14 16:26:26 +0800

    调整G410SD寄存器地址，增加CPRI相关寄存器定义

--- include/g410sd/g410sd_hw.h
+++ include/g410sd/g410sd_hw.h
@@ -5,7 +5,7 @@
  :: ::   ::       ::         ::         Project    : G410SD
  ::  ::  ::       ::           :::      FileName   : g410sd_hw.h
  ::   :: ::       ::             ::     Generate   : 2009.06.02
- ::    ::::       ::       ::      ::   Update     : 2010-09-25 16:42:27
+ ::    ::::       ::       ::      ::   Update     : 2010-10-12 12:03:16
 ::::    :::     ::::::      ::::::::    Version    : v0.2
 
 Description
@@ -35,7 +35,7 @@ Description
 #define IF_AGC_IRQ	IRQ_GPIO_00	//PCB pin name is LPC_IRQ0_N
 #define IF_ALC_IRQ	IRQ_GPIO_01	//PCB pin name is LPC_IRQ1_N
 #define POWER_IRQ	IRQ_GPI_03	//PCB pin name is LPC_IRQ2_N
-//#define RESERVE	IRQ_GPI_07	//PCB pin name is LPC_IRQ3_N
+#define CPRI_IRQ	IRQ_GPI_07	//PCB pin name is LPC_IRQ3_N
 
 
 /*
@@ -76,6 +76,18 @@ Description
 // IF FPGA BASE
 #define IF_FPGA_BASE	io_p2v(EMC_CS3_BASE)
 
+//CPRI registers
+#define CPRI_BASE	(IF_FPGA_BASE + 200)
+#define CPRI_VER	(CPRI_BASE + 0xF0)
+#define CPRI_RCNTN	(CPRI_BASE + 0xF1)
+#define CPRI_RCNT	(CPRI_BASE + 0xF2)
+#define CPRI_RBUF	(CPRI_BASE + 0xF3)
+#define CPRI_TBUFL	(CPRI_BASE + 0xF4)
+#define CPRI_TBUFH	(CPRI_BASE + 0xF5)
+#define CPRI_MAC0	(CPRI_BASE + 0xF6)
+#define CPRI_MAC1	(CPRI_BASE + 0xF7)
+#define CPRI_MAC2	(CPRI_BASE + 0xF8)
+
 // DPD registers
 #define DPD_ADDR_A	0x150
 #define DPD_WR_DATAL_A	0x151
@@ -100,14 +112,16 @@ Description
 #define CPLD_VER_BASE		(CPLD_BASE + 0x01)
 #define UBOOT_VER_BASE		(CPLD_BASE + 0x02)
 
-//Hardware Watchdog Module
-#define WATCHDOG_BASE		(CPLD_BASE + 0x03)
+//Status Module
+#define STATUS_BASE		(CPLD_BASE + 0x03)
 
-//Status&Detect Module
-#define STATUS_BASE		(CPLD_BASE + 0x04)
-#define DETECT_BASE		(CPLD_BASE + 0x05)
+//Detect Module
+#define DETECT_BASE		(CPLD_BASE + 0x04)
 #define DETECT_AGE		_BIT(0)
 #define DETECT_PA		_BIT(1)
+
+//Hardware Watchdog Module
+#define WATCHDOG_BASE		(CPLD_BASE + 0x05)
 
 //Reset Module
 #define RESET_BASE		(CPLD_BASE + 0x06)
@@ -123,8 +137,8 @@ Description
 
 
 // LMK2531 (Local OSC modules of A&B channel)
-#define TRX_LO_BASE		(CPLD_BASE + 0x09)
-#define DET_LO_BASE		(CPLD_BASE + 0x0a)
+#define TRX_LO_BASE		(CPLD_BASE + 0x08)
+#define DET_LO_BASE		(CPLD_BASE + 0x09)
 #define LMK2531_LD		_BIT(3)
 #define LMK2531_LE		_BIT(2)
 #define LMK2531_CLK		_BIT(1)
@@ -132,8 +146,8 @@ Description
 #define LMK2531_ALL		(LMK2531_LD | LMK2531_LE | LMK2531_CLK | LMK2531_DAT)
 
 // ADS62C17 (ADC modules)
-#define ADCA_BASE		(CPLD_BASE + 0x0b)
-#define ADCB_BASE		(CPLD_BASE + 0x0c)
+#define ADCA_BASE		(CPLD_BASE + 0x0A)
+#define ADCB_BASE		(CPLD_BASE + 0x0B)
 #define ADS62C17_SDOUT		_BIT(3)
 #define ADS62C17_SEN		_BIT(2)
 #define ADS62C17_SDATA		_BIT(1)
@@ -141,8 +155,8 @@ Description
 #define ADS62C17_ALL		(ADS62C17_SCLK | ADS62C17_SDATA | ADS62C17_SEN | ADS62C17_SDOUT)
 
 // DAC5682Z (DAC modules)
-#define DACA_BASE		(CPLD_BASE + 0x0d)
-#define DACB_BASE		(CPLD_BASE + 0x0e)
+#define DACA_BASE		(CPLD_BASE + 0x0C)
+#define DACB_BASE		(CPLD_BASE + 0x0D)
 #define DAC5682Z_SDOUT		_BIT(3)
 #define DAC5682Z_SEN		_BIT(2)
 #define DAC5682Z_SDATA		_BIT(1)
@@ -151,26 +165,19 @@ Description
 
 
 //FPGA configure Module
-#define FPGA_CFG_DAT_BASE	(CPLD_BASE + 0x0f)
+#define FPGA_CFG_DAT_BASE	(CPLD_BASE + 0x0E)
 
-#define FPGA_CFG_CTRL_BASE	(CPLD_BASE + 0x10)
+#define FPGA_CFG_CTRL_BASE	(CPLD_BASE + 0x0F)
 #define FPGA_CFG_CTRL_DONE	_BIT(5)
 #define FPGA_CFG_CTRL_INT	_BIT(4)
 #define FPGA_CFG_CTRL_CS	_BIT(1)
 #define FPGA_CFG_CTRL_PROG	_BIT(0)
 
-//Power MISC Module
-#define POWER_INT_BASE		(CPLD_BASE + 0x12)
-#define POWER_INT_ENA		_BIT(4)
-#define POWER_INT_ACT		_BIT(0)
+#define FPGA_CFG_CLK_BASE	(CPLD_BASE + 0x10)
 
-#define POWER_PEND_BASE		(CPLD_BASE + 0x13)
-
-#define POWER_STAT_BASE		(CPLD_BASE + 0x14)
-#define POWER_STAT_P28		_BIT(3)	//PA control Port
-
-#define SFP0_BASE		(CPLD_BASE + 0x15)
-#define SFP1_BASE		(CPLD_BASE + 0x16)
+//SFP Control Module
+#define SFP0_BASE		(CPLD_BASE + 0x11)
+#define SFP1_BASE		(CPLD_BASE + 0x12)
 #define SFP_SCL_OUT		_BIT(0)
 #define SFP_SDA_OUT		_BIT(1)
 #define SFP_SDA_IN		_BIT(2)
@@ -178,6 +185,16 @@ Description
 #define SFP_TXF			_BIT(4)
 #define SFP_RLOS		_BIT(5)
 #define SFP_DETN		_BIT(6)
+
+//Power MISC Module
+#define POWER_INT_BASE		(CPLD_BASE + 0x13)
+#define POWER_INT_ENA		_BIT(4)
+#define POWER_INT_ACT		_BIT(0)
+
+#define POWER_PEND_BASE		(CPLD_BASE + 0x14)
+
+#define POWER_STAT_BASE		(CPLD_BASE + 0x15)
+#define POWER_STAT_P28		_BIT(3)	//PA control Port
 
 #endif // __G410SD_HARDWARE_H__
 
