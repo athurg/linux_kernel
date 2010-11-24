9dbdac96c9d6d58870204a676ff9269a8710a62f
Athurg Gooth <athurg@gooth.cn>
2010-11-24 11:54:56 +0800

    精简内核，去掉不需要的架构

--- arch/sparc/include/asm/ptrace.h
+++
@@ -1,8 +0,0 @@
-#ifndef ___ASM_SPARC_PTRACE_H
-#define ___ASM_SPARC_PTRACE_H
-#if defined(__sparc__) && defined(__arch64__)
-#include <asm/ptrace_64.h>
-#else
-#include <asm/ptrace_32.h>
-#endif
-#endif
